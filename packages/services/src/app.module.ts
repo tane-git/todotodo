d98e7c02d59e182821e5bab0105a14a556936fd8
bz888 <ziyu.ben158@gmail.com>
2022-09-30 20:18:18 +1300

    mono

--- packages/services/src/app.module.ts
+++ packages/services/src/app.module.ts
@@ -11,3 +11,19 @@ import { TodoModule } from './todo/todo.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+
+// react TSX/JSX component
+export default function asdad (input: dasd) {
+  const stringasda = stringDate(input)
+  return  (
+    <p>
+      {stringasda}
+    </p>
+  )
+}
+
+//
+export function stringDate(input:aasdas): string {
+  return `asdadas${dasdas}`
+}
